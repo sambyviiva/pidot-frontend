87e57dfc092aa3d17c3cb1a7c4c0094dd5e4f739
Sami Valkonen <sami.valkonen2@gmail.com>
2023-10-22 22:09:16 +0300

    Get events from backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,34 @@
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
 
-function App() {
-  return (
-    <>
-
-    </>
-  )
+interface IEvent {
+  name: string;
+  startDate: Date;
+  endDate: Date;
 }
 
-export default App
+function App() {
+  const [events, setEvents] = useState<IEvent[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const e = await axios.get("https://pidot-event-api.onrender.com/event");
+      setEvents(e.data);
+    };
+
+    fetchData().catch(console.error);
+  }, []);
+  return (
+    <>
+      <div>events:</div>
+      <div>
+        {events?.map((e) => (
+          <div>{e.name}</div>
+        ))}
+      </div>
+    </>
+  );
+}
+
+export default App;
